c548c6350ad8b091c1ce650c732fec2e55692b54
Aryan Badola <aryanbadola431@gmail.com>
2022-11-19 00:36:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# printing the welcome message
+print("Welcome to the Band Name Generator")
+
+# Taking the city name and pet name as input from the user
+cityName = input("What's the name of the city you grew up in ?\n")
+petName = input("What's your pet name ?\n")
+
+# Printing the possible band name that can be generated
+print("Your band name could be " + cityName + " " + petName)
